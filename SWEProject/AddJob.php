269450a6e723f08bc6f2f9ae3d7631bdd403842f
Zareef Razzaque <zareefrazzaque@outlook.com>
2023-03-26 17:43:46 +0100

    The beginning of the post new job

---
+++ SWEProject/AddJob.php
@@ -0,0 +1,28 @@
+<?php include("path.php") ?>
+<?php include(ROOT . "/PHP Scripts/filter_jobs.php") ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="stylesheet" href="CSS/design.css" />
+	<link rel="stylesheet" href="CSS/jobs.css?v=1" />
+	<title>Jobs</title>
+</head>
+
+<body>
+	<?php include ROOT . "/PHP Scripts/header.php" ?>
+	
+	<div class="container content cont">
+		<div class="intro">
+			<h1 class="title">Post A New Job</h1>
+			<h2 class="subtitle">Create A New Job Posting For One Of Our Many Pet-Minders</h2>
+		</div>
+	</div>
+
+	<?php include ROOT . "/PHP Scripts/footer.php" ?>
+</body>
+
+</html>

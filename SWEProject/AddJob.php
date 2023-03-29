5915e2ee96a0b99982b5b3529fd805cf3b50cbf3
Zareef Razzaque <zareefrazzaque@outlook.com>
2023-03-29 20:24:53 +0100

    getting the basics down

--- SWEProject/AddJob.php
+++ SWEProject/AddJob.php
@@ -8,7 +8,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<link rel="stylesheet" href="CSS/design.css" />
-	<link rel="stylesheet" href="CSS/jobs.css?v=1" />
+	<link rel="stylesheet" href="CSS/AddJob.css?v=1" />
 	<title>Jobs</title>
 </head>
 
@@ -20,6 +20,10 @@
 			<h1 class="title">Post A New Job</h1>
 			<h2 class="subtitle">Create A New Job Posting For One Of Our Many Pet-Minders</h2>
 		</div>
+
+        <div>
+
+        </div>
 	</div>
 
 	<?php include ROOT . "/PHP Scripts/footer.php" ?>

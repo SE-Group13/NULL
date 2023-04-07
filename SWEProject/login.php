962c5cf81175dd5b21aeefcfea636bbea07e1d65
Leon Liu <91329786+leon-lin-liu@users.noreply.github.com>
2023-04-07 21:36:55 +0100

    Tweaked login.php and path.php
    I modified these scripts to only start session/define root if a session doesn't exist/root is not defined. 
    As index is always the beginning (for our purposes anyway) I started a session there for test purposes. Realistically the login script will start a session when logging in.

--- SWEProject/login.php
+++ SWEProject/login.php
@@ -1,11 +1,9 @@
 <?php
 
-
-session_start();
-// $_SESSION['id'] = $user['id'];
-$_SESSION['user_id'] = 0;
-
-
-
+    if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
+    // $_SESSION['id'] = $user['id'];
+    $_SESSION['user_id'] = 0;
 
 ?>

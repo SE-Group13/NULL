329a32b51bba2168870add6de6d3949c49b0e8b8
Lok <ec211014@qmul.ac.uk>
2023-04-09 22:35:17 +0100

    merged review payment and moveToPayment scripts

--- SWEProject/reviewPayment.php
+++ SWEProject/reviewPayment.php
@@ -1,8 +1,45 @@
-<?php 
-    include ("path.php");
-    session_start();
-?>
+<?php
+session_start();
+//this section here deals with the upload of the users image into the petimages folder
 
+$petImagesPath ='Images\PetImages\\';
+
+//the first two if statements make sure there are no errors when the image was uploaded 
+if (isset($_FILES['jobImage'])) {
+    if($_FILES['jobImage']['error'] === 0){
+   
+
+    $imageName = explode('.',$_FILES['jobImage']['name']);
+    $imageExt = end($imageName);
+    $newId =uniqid().'.'.$imageExt;
+    $newImageAddress = $petImagesPath.$newId; 
+
+    move_uploaded_file($_FILES['jobImage']['tmp_name'],$newImageAddress);
+
+    }
+} else {
+    $image = 'image was not pulled'; // or some default value
+}
+
+$data =  json_decode( file_get_contents('Data\jobs.json', true));
+$count = count($data);
+
+
+$_SESSION['jobdata'] = [$count, $_POST["jobTitle"],$_POST["jobDiscription"], $_POST["petType"],  $_POST["city"], $_POST["neighbourhood"], $_POST["date"] ,$_POST['deposit'], $newId,];
+
+$_SESSION['data'] = [
+    "id" => $count,
+    "title" => $_POST["jobTitle"],
+    "description" => $_POST["jobDiscription"],
+    "pet_type"=> $_POST["petType"],
+    "city" => $_POST["city"],
+    "neighbourhood" => $_POST["neighbourhood"],
+    "date" => $_POST["date"],
+    "budget"=> $_POST['deposit'],
+    "image"=>$newId,
+];
+?>
+<?php include("path.php") ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -42,7 +79,7 @@
             ?>
         </div>
         <form action="PHP Scripts\processPayment.php" method="post">
-            <button formaction="history.back()" class="edit">Edit</button>
+            <button onclick="history.back()" type="button" class="edit">Edit</button>
             <button type="submit" class="submit">Submit and pay with PayPal</button>
             <?php
                 echo '<input type="hidden" id="price" name="price" value="' . $_SESSION['data']['budget'] . '">';
@@ -51,5 +88,5 @@
             ?>
         </form>
     </div>
-    <?php include ROOT . "/PHP Scripts/footer.php" ?>
+    <?php include "footer.php" ?>
 </body>

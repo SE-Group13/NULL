b583426a6e2a9888ca2ee22188fe6e18e53d6443
OLJEN <94927686+oljen@users.noreply.github.com>
2023-04-10 18:17:21 +0100

    Update loginPage.php

--- SWEProject/loginPage.php
+++ SWEProject/loginPage.php
@@ -11,7 +11,7 @@ include("login.php");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="CSS/design.css">
+    <link rel="stylesheet" href="CSS/login.css">
     <link href='https://fonts.googleapis.com/css?family=Zen Kaku Gothic Antique' rel='stylesheet'> <!--Imports a Google Font-->
     <title>Login</title>
 </head>
@@ -21,21 +21,26 @@ include("login.php");
     <div class="login">
         <div class = "homeBoxWrapper">
                 <div class = "homeBox">
-                    <h1>Log in</h1>
+                    <h1 class ="head" >Welcome!</h1>
+                    <br>
                     <form action="loginPage.php" method="POST">
                         <label for="username">Username:</label>
                         <input type="text" id="username" name="username" required><br>
-
+                        <br>
                         <label for="password">Password:</label>
                         <input type="password" id="password" name="password" required><br>
-
-                        <input type="submit" value="Login">
+                        <br>
+                        <input class = "logbutton" type="submit" value="Login">
                     </form>
                 </div>
+                <br>
+                <br>
             <img src="Images/indexPhotos/homeBoxPhotos/trustpilot_v2.svg" alt="">
         </div>
     </div>
 
+
+    
     <?php include_once (ROOT . '/PHP Scripts/footer.php') ?> <!-- Footer is always the same -->
 </body>
 </html>

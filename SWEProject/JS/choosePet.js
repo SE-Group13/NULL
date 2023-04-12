db611046ea748ab70bcbdba1cf439954cd99e134
OLJEN <94927686+oljen@users.noreply.github.com>
2023-04-12 12:47:38 +0100

    Update choosePet.js

--- SWEProject/JS/choosePet.js
+++ SWEProject/JS/choosePet.js
@@ -1,6 +1,8 @@
 const petOwnerRadioButton = document.querySelector('#pet-owner');
 const petTypeDropdown = document.querySelector('#pet-type-dropdown');
 const petSitterRadioButton = document.querySelector('#pet-sitter');
+const registerButton = document.querySelector('button[name="submit"]');
+const recaptcha = document.querySelector('.g-recaptcha');
 
 petOwnerRadioButton.addEventListener('change', function() {
   if (this.checked) {
@@ -10,9 +12,18 @@ petOwnerRadioButton.addEventListener('change', function() {
   }
 });
 
-
 petSitterRadioButton.addEventListener('change', function() {
-    if (this.checked) {
-      petTypeDropdown.style.display = 'none';
-    }
-  });
+  if (this.checked) {
+    petTypeDropdown.style.display = 'none';
+  }
+});
+
+registerButton.addEventListener('click', function(event) {
+  if (recaptcha && !grecaptcha.getResponse()) {
+    alert('Please complete the reCAPTCHA verification');
+    event.preventDefault();
+  } else if (!document.getElementById('document').value) {
+    alert('Please upload a document');
+    event.preventDefault();
+  }
+});
